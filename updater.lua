adfb3a755a8371bfe758adecdeb5873a65d6a6dd
Dani Lacasito <Daniel>
2021-01-08 22:09:47 +0100

    create a uname program

--- updater.lua
+++ updater.lua
@@ -1,28 +1,37 @@
 print("Starting Version check...")
 actual = fs.open("/os_version", "r")
-actual.readLine()
-actualcommit = actual.readLine()
+actualcommit = actual.readAll()
+
 shell.run("wget https://raw.githubusercontent.com/danilacasito/testccos/latest/os_version /temporal")
 net = fs.open("/temporal","r")
-net.readLine()
-nextcommit = net.readLine()
+nextcommit = net.readAll()
 if actualcommit == nextcommit then
     print("No versions are available")
     shell.run("rm /temporal")
 else
     print("A new Version is available")
-    next.close()
+    net.close()
     next = fs.open("/temporal", "r")
-    for lines in next do
-        print(lines)
-    end
-    print("Do you want to update?")
+    print("Do you want to update? [Yes,No,Read]")
     e = read()
     if e == "yes" then
         print("Downloading latest installer")
         shell.run("wget https://raw.githubusercontent.com/danilacasito/testccos/latest/installer.lua /installer.lua")
         shell.run("rm /temporal")
         shell.run("/installer.lua update")
+    elseif e == "read" then
+        shell.run("edit /temporal")
+        print("yes or no?")
+        p = read()
+        if p == "yes" then
+            print("Downloading latest installer")
+            shell.run("wget https://raw.githubusercontent.com/danilacasito/testccos/latest/installer.lua /installer.lua")
+            shell.run("rm /temporal")
+            shell.run("/installer.lua update")
+        else
+            print("Ok, Canceled")
+            shell.run("rm /temporal")
+        end
     else
         print("Ok, Canceled")
         shell.run("rm /temporal")
